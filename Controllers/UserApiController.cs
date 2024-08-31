400fced31db9777f71ca278cd4ddff916ca24cc6
asp <asp@LAB26-16>
2024-08-31 16:25:58 +0500

    Add project files.

---
+++ Controllers/UserApiController.cs
@@ -0,0 +1,35 @@
+﻿using Asp.net_Api_Practice.Models;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.net_Api_Practice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserApiController : ControllerBase
+    {
+        private readonly AspApiPracticeContext context;
+
+        public UserApiController(AspApiPracticeContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public JsonResult GetApiData()
+        {
+            var user = context.UserInfos.ToList();
+            return new JsonResult(Ok(user));
+        }
+        [HttpPost]
+        public JsonResult PostApiData(UserInfo newUser)
+        {
+            
+           var newuser =  context.UserInfos.Add(newUser);
+            context.SaveChanges();
+
+            return new JsonResult(Ok(newUser));
+           
+        }
+    }
+}
---
+++ Models/AspApiPracticeContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Asp.net_Api_Practice.Models
+{
+    public partial class AspApiPracticeContext : DbContext
+    {
+        public AspApiPracticeContext()
+        {
+        }
+
+        public AspApiPracticeContext(DbContextOptions<AspApiPracticeContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<UserInfo> UserInfos { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+       
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserInfo>(entity =>
+            {
+                entity.HasKey(e => e.UserId)
+                    .HasName("PK__UserInfo__1788CC4CC19EFF75");
+
+                entity.ToTable("UserInfo");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Phone)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UserName)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
