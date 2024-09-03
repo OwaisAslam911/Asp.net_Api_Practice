876ccbcfd43e638ac1f2ffdcc7ae7497e4bb9c8c
Eijaz01 <ekashif05@gmail.com>
2024-09-03 16:19:22 +0500

    owais

--- Controllers/UserApiController.cs
+++ Controllers/UserApiController.cs
@@ -31,5 +31,40 @@ namespace Asp.net_Api_Practice.Controllers
             return new JsonResult(Ok(newUser));
            
         }
+        [HttpGet("{id}")]
+        public JsonResult getuser(int id)
+        {
+            var userData = context.UserInfos.FirstOrDefault(Options => Options.UserId == id);
+            return new JsonResult (Ok(userData));
+
+        }
+        [HttpDelete("{id}")]
+        public JsonResult DeleteApiData(int id)
+        {
+            var userdata = context.UserInfos.Find(id);
+            context.UserInfos.Remove(userdata);
+            context.SaveChanges();
+            return new JsonResult(Ok(userdata));
+        }
+        //[HttpGet("{id}")]
+        //public JsonResult getupdate (int id)
+        //{
+        //    var userData = context.UserInfos.FirstOrDefault(option => option.UserId == id);
+        //    return new JsonResult(Ok(userData));
+
+        //}
+        [HttpPut("{id}")]
+        public JsonResult UpdateUser(UserInfo newUser ,int id)
+        {
+            var userdata = context.UserInfos.FirstOrDefault(option => option.UserId == id);
+            userdata.UserId = newUser.UserId;
+            userdata.UserName = newUser.UserName;
+            userdata.Email = newUser.Email;
+            userdata.Phone = newUser.Phone;
+
+            context.UserInfos.Update(newUser);
+            context.SaveChanges();
+            return new JsonResult(Ok(userdata));
+        }
     }
 }
